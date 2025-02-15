fb94a99185a0e187f2d7802d347582c859419ae2
Delon Pratama <delonpratama1009@gmail.com>
2025-02-15 22:22:12 +0700

    changed config baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', 
 // $_SERVER['SCRIPT_NAME']);
 
-$config['base_url'] = 'http://prolificweb.netlify.app';
+$config['base_url'] = 'https://prolificweb.netlify.app/';
 /*
 |--------------------------------------------------------------------------
 | Index File
